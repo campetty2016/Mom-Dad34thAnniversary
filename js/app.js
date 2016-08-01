b676030b68da7fec7ac38a33659179b0e91bd17f
campetty2016 <cmpetty17@gmail.com>
2016-07-31 20:47:17 -0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,44 @@
+window.onload = (function () {
+	$(".Loader").delay(4000).fadeOut("slow");
+});
+
+$(document).ready(function () {
+	ClickPic();
+	OnTargetHover();
+});
+
+function ClickPic() {
+
+	var ImgNum = 0;
+	var ImageCount = 56;
+
+	$(".ImageDate").click(function (e) {
+		e.preventDefault();
+
+		if (ImgNum <= ImageCount) {
+			ImgNum += 1;
+			location.hash = "#Img" + ImgNum;
+		}
+	});
+
+}
+
+var Img = $(".ImageDate");
+
+Img.hover(function (e) {
+	$(".Date").css("opacity", "1");
+});
+
+Img.mouseenter(function (e) {
+	$(".Date").css("opacity", "1");
+});
+
+Img.click(function (e) {
+	$(".Date").css("opacity", "1");
+});
+
+Img.mouseleave(function () {
+	$(".Date").css("opacity", "0", "overflow", "hidden");
+});
+
+}
